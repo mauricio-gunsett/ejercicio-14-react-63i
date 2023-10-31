ff5519cb03dc534f0c6722006278fd603d73c9be
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-10-30 22:52:58 -0300

    Agrego navbar y footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-dark">
+      <section className="container text-light py-5">
+        <p>&copy; Todos los derechos reservados</p>
+        <h5>La cocinita de Mau</h5>
+      </section>
+    </footer>
   )
 }
 export default Footer
--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,6 +1,50 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <header>Navbar</header>
-  )
-}
-export default Navbar
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            La Cocina
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarLaCocinita"
+            aria-controls="navbarLaCocinita"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarLaCocinita">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link ${({ isActive }) =>
+                    isActive ? "active" : ""}`}
+                  aria-current="page"
+                  to="/"
+                >
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link ${({ isActive }) =>
+                    isActive ? "active" : ""}`}
+                  to="/login"
+                >
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
